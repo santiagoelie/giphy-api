24fac5ca09b1454501fc2fd36ed1bdcbf3b1f8c6
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-05-06 10:16:02 -0700

    edit js file

--- js/giphy.js
+++ js/giphy.js
@@ -1,18 +1,16 @@
-//Working jQuery Code
 const gifForm = $("#gif-form");
 
 gifForm.submit(e => {
-    e.preventDefault();
-    const searchTerm = $(".search").val();
-    const url = `https://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=80&api_key=Ab2k8Getoartsbv2vWXQ22MNQ7c6Ip10`;
-    $.get(url)
-        .done(resp => {
-            showGiphs(resp.data.slice(0, 40));
-        })
+  e.preventDefault();
+  const searchTerm = $(".search").val();
+  const url = `https://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=80&api_key=3mIxmBZUIIPyb8R69gtxaW8Hsh74dFKV`;
+  $.get(url)
+    .done(resp => {
+      showGiphs(resp.data.slice(0, 40));
+    })
     .fail(console.log);
 });
 
-//Working jQuery Code
 function showGiphs(dataArray) {
   const results = document.querySelector(".results");
   let output = "";
