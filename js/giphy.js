ce2ffcf49928b6b2e0be4bda8548c37826f4b931
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-05-05 22:24:07 -0700

    modify js file

--- js/giphy.js
+++ js/giphy.js
@@ -3,7 +3,7 @@ const gifForm = $("#gif-form");
 gifForm.submit(e => {
     e.preventDefault();
     const searchTerm = $(".search").val();
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=Ab2k8Getoartsbv2vWXQ22MNQ7c6Ip10&q=dog&limit=25&offset=0&rating=G&lang=en`;
+    const url = `https://api.giphy.com/v1/gifs/search?&q=${searchTerm}&limit=80&api_key=Ab2k8Getoartsbv2vWXQ22MNQ7c6Ip10`;
     $.get(url)
         .done(resp => {
             showGiphs(resp.data.slice(0, 10));
